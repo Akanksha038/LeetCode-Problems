8ae1ad93b8214f6ad2f954d91c23b6b439a64ece
Akanksha Gupta <114081059+Akanksha038@users.noreply.github.com>
2024-01-02 12:51:12 +0530

    Time: 4 ms (6.96%), Space: 6.3 MB (32.17%) - LeetHub

---
+++ 2513-minimize-the-maximum-of-two-arrays/2513-minimize-the-maximum-of-two-arrays.cpp
@@ -0,0 +1,38 @@
+#define ll long long
+class Solution {
+public:
+    bool satisfy(ll divisor1 ,ll divisor2,ll c1,ll c2,ll mid){
+        ll div1=mid/divisor1;
+        ll div2=mid/divisor2;
+        ll notdiv1=mid-div1;
+        ll notdiv2=mid-div2;
+        ll notdivboth=mid-mid/(lcm(divisor1,divisor2));
+        if(notdiv1>=c1 && notdiv2>=c2&& notdivboth>=c1+c2){
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+    
+    
+    int minimizeSet(int divisor1, int divisor2, int uniqueCnt1, int uniqueCnt2) {
+        ll ans=INT_MAX, l=1,h=INT_MAX;
+        while(l<=h){
+            ll mid=l+(h-l)/2;
+            if(satisfy(divisor1,divisor2,uniqueCnt1,uniqueCnt2,mid)){
+                ans=min(ans,mid);
+                h=mid-1;
+            }
+            else{
+                l=mid+1;
+                
+            }
+            
+        }
+        return ans;
+        
+    }
+};
